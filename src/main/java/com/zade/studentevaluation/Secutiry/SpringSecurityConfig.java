e4ff338601a540a9e81b22a42133552d50e2126e
Dheeraj Satyavarapu <dheerajdheeru126s@gmail.com>
2024-02-18 23:15:53 +0530

    CORS Students Fix

--- src/main/java/com/zade/studentevaluation/Secutiry/SpringSecurityConfig.java
+++ src/main/java/com/zade/studentevaluation/Secutiry/SpringSecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
 
     @Autowired
@@ -31,7 +35,7 @@ public class SpringSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
 
      httpSecurity.csrf(AbstractHttpConfigurer::disable)
-             .cors(AbstractHttpConfigurer::disable)
+             .cors(Customizer.withDefaults())
              .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
                      .requestMatchers("/students/**").authenticated()
                      .requestMatchers("/auth/login").permitAll().requestMatchers("/evaluation/user/register").permitAll().anyRequest().authenticated())
